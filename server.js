6d49bbbdaa17fbd160025c62c76ef0f6f13a699f
Forest Traylor <foresttraylor@gmail.com>
2021-07-06 00:02:06 -0700

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -4,6 +4,31 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// 8 ball model with fortune string
+
+const fortune = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes definitely",
+  "You may rely on it",
+  "As I see it yes",
+  "Most likely",
+  "Outlook good",
+  "Yes",
+  "Signs point to yes",
+  "Reply hazy try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful"
+];
+
 // exercise 1
 
 app.get('/greeting/:name', (req, res) => {
@@ -16,6 +41,10 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
   res.send`${totalBill}`;
 });
 // exercise 3
+app.get('/magic/:phrase', (req, res) => {
+  const fortune = Math.floor(Math.random() * responses.length);
+  res.send(`<h2>${fortune[answer]}</h2>`);
+});
 
 // exercise 4
 
